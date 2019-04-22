3cdc9cd39bfafc3689a24f6949a8b93570211cbe
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-04-22 19:06:39 -0400

    Server Create + Req, Res

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs'); // file system module
+const http = require('http'); 
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end('This is the response');
+}); // create server
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests now');
+});
+
