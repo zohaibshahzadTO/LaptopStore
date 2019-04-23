23ca449cdc2791d33af801160c4f2d495c475e4c
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-04-22 20:23:18 -0400

    Routing Implemented in Node Index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const fs = require('fs'); // file system module
 const http = require('http'); 
+const url = require('url'); // enables routing
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end('This is the response');
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if (pathName === '/products' || pathName === '/' ) {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end('This is the PRODUCTS page');
+    }   
+    else if (pathName === '/laptops' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end(`This is the LAPTOPS page for laptop ${id}!`);
+    }
+    else {
+        res.writeHead(404, { 'Content-type': 'text/html'});
+        res.end('URL was not found on the server!');
+    }
 }); // create server
 
 server.listen(1337, '127.0.0.1', () => {
